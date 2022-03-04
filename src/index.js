83b5f310ba219c86afdf09127ceec511f235c50b
asher scott <asher.scott6@gmail.com>
2022-03-04 17:06:24 -0600

    all deliverables done: 1 hour 07 minutes. New toy likes only persist after refresh

--- src/index.js
+++ src/index.js
@@ -13,3 +13,103 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+const toyCollecDiv = document.querySelector('#toy-collection')
+
+
+
+const url = 'http://localhost:3000/toys'
+let toys;
+
+
+fetch(url)
+.then(res => res.json())
+.then(toysArray => {
+  toys = toysArray
+  renderToys(toys)
+})
+
+
+
+// DELIVERABLE 1
+function renderToys(toys) {
+  toys.forEach(toy => makeCard(toy))
+}
+
+
+
+function makeCard(toy) {
+  const div = document.createElement('div')
+  div.className = 'card'
+
+  const h2 = document.createElement('h2')
+  h2.textContent = toy.name
+
+  const img = document.createElement('img')
+  img.src = toy.image
+  img.className = 'toy-avatar'
+
+  const p = document.createElement('p')
+  p.textContent = `${toy.likes} Likes`
+
+  const btn = document.createElement('button')
+  btn.textContent = 'Like ❤️'
+  btn.className = 'like-btn'
+  btn.id = toy.id
+
+
+
+  btn.addEventListener('click', () => increaseLike(p, toy))
+
+  
+  toyCollecDiv.append(div)
+
+  div.append(h2)
+  div.append(img)
+  div.append(p)
+  div.append(btn)
+}
+
+
+
+// DELIVERABLE 2
+const inputs = document.querySelectorAll('input')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  newToys()
+})
+function newToys() {
+  const newToy = {
+    image: inputs[1].value,
+    likes: 0,
+    name: inputs[0].value,
+  }
+  
+  fetch(url, {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(newToy)
+  })
+
+  makeCard(newToy)
+}
+
+
+
+// DELIVERABLE 3
+function increaseLike(p, toy) {
+  toy.likes = parseInt(p.innerText) + 1
+  p.textContent = `${toy.likes} Likes`
+
+
+
+  fetch(url + '/' + toy.id, {
+    method: 'PATCH',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(toy)
+  })
+}
